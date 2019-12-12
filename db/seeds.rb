891484ce62f0573943f79410496280b6059d9819
Constance Graftieaux <constance.graftieaux@gmail.com>
2019-12-12 17:29:05 -0300

    Add information to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+constance = User.create!(first_name:"Constance", last_name:"Graftieaux", address:"Av. Bartolomeu Mitre, 1314 ", photo:"https://upload.wikimedia.org/wikipedia/commons/7/75/Joseph_Morgan_at_PaleyFest_2014.jpg", email:"constance@hotmail.com", password:"123456")
+
