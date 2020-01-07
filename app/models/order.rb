2ba9ae6edefe638bf5ff152fc0c5f60a01c1f896
Constance Graftieaux <constance.graftieaux@gmail.com>
2020-01-07 12:11:09 -0300

    Add some features to user and order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :meal
   #belongs_to :customer
+
+  def available_quantities
+    # TODO return the real available quantity based on the stock
+    (1..8)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :orders
   belongs_to :role
+  has_many :orders, dependent: :destroy
+
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true
+  #validates :address, presence: true
+  #validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+def average_rating
+    counter = 0
+    meals = Meal.where(shop_id: self)
+    order_count = 0
+
+    meals.each do |meal|
+      order_count += meal.orders.count
+      rated_orders = meal.orders
+      rated_orders.each do |order|
+        counter += order.rating
+      end
+    end
+
+    if counter.zero?
+      average_rating = 0
+    else
+      average_rating = (counter / order_count).to_i
+    end
+    average_rating
+  end
+
 end
